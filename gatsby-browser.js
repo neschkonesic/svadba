33d2d9de22d5fa9087837375cf063e0d0fd2170d
neschkonesic <114104457+neschkonesic@users.noreply.github.com>
2022-09-26 20:38:47 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
     `Стигле су нове вести-објаве. ` +
-      `Желиш да видиш најновије вести?`
+      `Желиш да видиш најновије вести са нашег свадбеног сајта ?`
   )
 
   if (answer === true) {
